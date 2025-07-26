a2f1f4bde90b1e83949b708fc0216a3eb6af5e91
fiyin12345 <volaiyanda@gmail.com>
2025-07-26 19:01:12 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -5,3 +5,22 @@ class movie {
         this.year=year
     }
 }
+document.querySelector("#f").addEventListener('submit', addamovie)
+
+function addamovie (e) {
+e.preventDefault()
+const title=document.querySelector("#t").value ;
+const director=document.querySelector("#d").value ;
+const year=document.querySelector("#y").value ;
+const Movie=new movie(title, director, year)
+proceed(Movie)
+document.querySelector("#t").value=""
+document.querySelector("#d").value=""
+document.querySelector("#y").value=""
+}
+proceed=(movie)=>{ 
+    const list=document.getElementById("tablebody")
+    const row=document.createElement("tr")
+    row.innerHTML="<td>"+movie.title+"</td> <td>"+movie.director+"</td> <td>"+movie.year+"</td>"
+    list.appendChild(row)
+}
